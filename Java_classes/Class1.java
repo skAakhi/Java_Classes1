afb1856bb76a58715c7f425a29eda3cdf3ca4ef7
Aakhib Shaik <114942516+skAakhi@users.noreply.github.com>
2025-05-03 19:19:41 +0530

    Add files via upload

---
+++ Java_classes/Class1.java
@@ -0,0 +1,21 @@
+public class Hello{
+
+public static void main(String[] args)
+{
+System.out.println("Hello dipen and jyothi");
+}
+}
+
+
+
+
+
+
+public class Demo{
+
+public static void main(String[] args)
+{
+System.out.println("Demo classes");
+}
+}
+
---
+++ Java_classes/Hello.java
@@ -0,0 +1,33 @@
+public class Hello{
+
+public static void main(String[] args)
+{
+boolean flag=true;
+byte a = 127;
+char ch='A';
+int  i = 100;
+float f = 3.14499976f;
+double d = 4.66;
+System.out.println("Your a value is :"+a);
+System.out.println(ch);
+System.out.println(i);
+System.out.println(f);
+System.out.println(d);
+//---------------------->
+System.out.printf("Your a value is :%d",a);
+
+}
+}
+
+//0 --- 255 //Asscii---->char[0--255]----->beyond the range----> 256----->unicode value charaters-----
+
+
+
+
+class Demo{
+
+public static void main(String[] args)
+{
+System.out.println("Demo classes");
+}
+}
